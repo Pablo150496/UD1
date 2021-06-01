4041df1f7bdf85857f5da5f68ba273053d420ddf
Pablo150496 <85115815+Pablo150496@users.noreply.github.com>
2021-06-01 12:39:28 +0200

    Add files via upload

---
+++ namespace ConsoleApplication5.cs
@@ -0,0 +1,11 @@
+namespace ConsoleApplication5
+{
+    class Prgram
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("HOLA MUNDO!!!");
+            System.Console.ReadKey(); //Para la ejecución hasta presionar tecla
+        }
+    }
+}
